15d3d840562359094c186f0beabaf18824aaf060
Yann Farris <hello@yannfarris.com>
2023-02-04 22:42:58 +0300

    change url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  url:'https://yaychli-api.sendmala.com/',
+  url:'https://yaychli-api.sendmala.com/', 
   production: true,
   appVersion: 'v8.0.38',
   USERDATA_KEY: 'authf649fc9a5f55',
